419d1fe1f29894810a8486724396ff86e29363c7
Dmitry Petruk <bavaria95@gmail.com>
2015-10-18 10:17:58 +0200

    code for detecting ROIs(only with openCV now)

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from otsu import otsu
 
 # 0 - black
 # 255 - white
@@ -10,7 +11,9 @@ def rgb2gray(frame):
     return np.inner(frame, [0.2989, 0.587, 0.114]).astype(np.uint8)	
 
 def binarize(gray):
-    return np.where(gray < THRESHOLD, gray, 255)
+    threshold = otsu(gray)
+    return np.where(gray < threshold, gray, 255)
+
 
 cap = cv2.VideoCapture(0)
 
@@ -20,8 +23,8 @@ while(True):
     
     gray = rgb2gray(frame)
     gray_bin = binarize(gray)
-    # t = otsu(gray_bin)
-    
+
+    # edges = cv2.Canny(gray_bin,100,200)
     # Display the resulting frame
     cv2.imshow('frame', gray_bin)
     if cv2.waitKey(1) & 0xFF == ord('q'):
